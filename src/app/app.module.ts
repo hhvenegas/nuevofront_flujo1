c57c83a3a71f7a56956b58ecc8a2acb55013f2ac
alfredo anchondo porras <alfredo.anchondo@populusfintech.com>
2018-03-16 12:41:39 -0600

    validation home quotation
    Signed-off-by: alfredo anchondo porras <alfredo.anchondo@populusfintech.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
@@ -26,7 +28,11 @@ import { ProcesopagoComponent } from './procesopago/procesopago.component';
     ProcesopagoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+declare var jquery:any;
+declare var $ :any;
+import {FormBuilder,FormGroup,FormControl,Validators,NgForm} from '@angular/forms'
+
 
 @Component({
   selector: 'app-homepage',
@@ -7,8 +12,94 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 	title = 'Sxkm';
-  	constructor() { }
+  years : any ;
+  makers : any ;
+  models: any;
+  model_first :string="";
+  versions: any;
+  years_selected: any;
+  maker_select: any;
+  model_select: any;
+  version_select: any;
+  zip_code_select: any;
+  birth_date_select: any;
+  gender_select: any;
+  email_select: any;
+  cellphone_select: any;
+  quotationForm:FormGroup;
 
-  	ngOnInit() {}
+  	constructor(private http: HttpClient, private frmbuilder:FormBuilder) {
+      this.get_makers();
+      this.get_years();
+     }
+
+     get_models() {
+       if(this.years_selected  && this.maker_select ){
+         this.http.get('http://52.91.226.205/api/v1/quotations/models?year='+this.years_selected+'&maker='+this.maker_select+'').subscribe(data => {
+           this.models = data;
+           console.log(data)
+         },
+         error => console.log(error)  // error path
+       );
+       }
+    }
+
+    get_version(year, maker, model) {
+     this.http.get('http://52.91.226.205/api/v1/quotations/model_versions?year='+year+'&maker='+maker+'&model='+model+'').subscribe(data => {
+       this.versions = data;
+       console.log(data)
+     },
+     error => console.log(error)  // error path
+    );
+    }
+
+    get_years() {
+      this.http.get('http://52.91.226.205/api/v1/quotations/years').subscribe(data => {
+        this.years = data;
+        console.log(data);
+      },
+      error => console.log(error)  // error path
+      );
+    }
+
+    get_makers() {
+     this.http.get('http://52.91.226.205/api/v1/quotations/makers').subscribe(data => {
+        this.makers = data;
+       console.log(this.makers);
+
+     },
+     error => console.log(error)  // error path
+    );
+    }
+
+
+
+    send_quotation(){
+      let form_data = {
+        "email": this.email_select,
+        "maker_name": this.maker_select,
+        "maker_id": this.maker_select,
+        "year": this.years_selected,
+        "car_model_name": this.model_select,
+        "car_model_id": this.model_select,
+        "version_name": this.version_select.name,
+        "version_id": this.version_select.id,
+        "zipcode": this.zip_code_select,
+        "birth_date": this.birth_date_select,
+        "gender": this.gender_select,
+        "telephone": this.cellphone_select
+      }
+      this.http.post('http://52.91.226.205/api/v1/quotations/create_quotation',form_data).subscribe(data => {
+        console.log(data);
+
+      },
+      error => console.log(error)  // error path
+     );
+    }
+
+
+  	ngOnInit() {
+
+    }
 
 }
