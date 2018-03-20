6370e239849b981d9d423a7bb705228906281d44
paulinapopulusfintech <paulina.guerrero@populusfintech.com>
2018-03-20 15:39:28 -0600

    Cotizaciones A y B con variabe tipo para cambiar de vista segun sea el caso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
 	{ 
 		path: 'cotizaciones/:tipo', 
 		component: CotizacionesComponent ,
-		data: { }
+		data: {}
 	},
 	{ path: 'cotizacion/:id',      component: CotizacionesComponent },
 	{
