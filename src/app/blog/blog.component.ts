8243e50e3dd78252b57b780cba420d2006552124
paulinapopulusfintech <paulina.guerrero@populusfintech.com>
2018-07-11 10:32:39 -0500

    Cambios blog 2

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -8,34 +8,10 @@ import { Router } from '@angular/router';
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent implements OnInit {
-	post_url:any      = "";
-	categoria: any = "";
-	posts: any = Array();
 	constructor(private router : ActivatedRoute,private router2 : Router) {
   	}
 
 	ngOnInit() {
-		this.get_post();
-	}
-
-	get_post(){
-		this.posts = [
-			{
-				id: 1,
-				category: "Tecnología",
-				date: "Julio 2018",
-				title: "Con SXKM saca tu seguro de la guantera y llévalo contigo a todas partes.",
-				content: '',
-				image: "sxkm-ofrece-cobertura-amplia-en-toda-la-republica-mexicana.jpg",
-				tags:[ 
-					{ tag: "INSURTECH" },
-					{ tag: "PAYD" },
-					{ tag: "PIA"}
-				],
-				url_category: "tecnologia",
-				url_post: "con-sxkm-saca-tu-seguro-de-la-guantera-y-llevalo-contigo-a-todas-partes"
-			}
-		]
 	}
 
 }
