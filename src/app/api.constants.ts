70f4da137fe743e984819d251e7d3174c48e8dad
paulinapopulusfintech <paulina.guerrero@populusfintech.com>
2018-07-18 13:26:37 -0500

    Cambio de url de rails de produccion al rails de qa

--- src/app/api.constants.ts
+++ src/app/api.constants.ts
@@ -1,7 +1,10 @@
 export class Api {
   //public static API_DOMAIN = 'http://107.21.9.43/';
-  public static API_DOMAIN = 'https://prod2.sxkm.mx/';
-  //public static API_DOMAIN = 'http://localhost:3000/';
+  //public static API_DOMAIN = 'https://prod2.sxkm.mx/';
+
+  //Q.A RAILS
+  public static API_DOMAIN = 'http://54.175.210.98/';
+  
   public static TIPO_FLUJO=2;//Si es caso A tipo_flujo es 1 si es caso B tipo_flujo es 2
 
   public static HOMEPAGE_V2 = '/seguro-de-auto-por-kilometro';
