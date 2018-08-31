9f97569a0a62715d8cadc16130acd84f46ae8d05
paulinapopulusfintech <paulina.guerrero@populusfintech.com>
2018-08-31 12:13:57 -0500

    urls

--- src/app/api.constants.ts
+++ src/app/api.constants.ts
@@ -3,7 +3,7 @@ export class Api {
 	public static API_DOMAIN = 'https://www.sxkm.mx/v2/';
 	public static API_DOMAIN2 = 'https://www.sxkm.mx/api/v1/';
 	public static API_DOMAIN_ZIPCODES = 'https://sxkm.mx/quotations/';
-  	public static DEVELOPMENT_DOMAIN = "https://www.sxkm.mx/api/v2/quotations/";
+  	public static DEVELOPMENT_DOMAIN = "https://www.sxkm.mx/v2/api/v1/quotations/";
 
   	public static TIPO_FLUJO=2;//Si es caso A tipo_flujo es 1 si es caso B tipo_flujo es 2
   	public static HOMEPAGE_V2 = '/seguro-de-auto-por-kilometro';
