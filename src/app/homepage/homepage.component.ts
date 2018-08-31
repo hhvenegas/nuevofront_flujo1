9b81c697068bb0b99d6e11843ab34f9e4c9880e1
paulinapopulusfintech <paulina.guerrero@populusfintech.com>
2018-08-31 12:57:04 -0500

    ok

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -55,6 +55,9 @@ export class HomepageComponent implements OnInit {
       { name: 'keywords', content: 'seguro de auto, sxkm, seguro por kilometro, seguro de auto por kilómetro, seguro de auto por kilometro, seguro de auto, cotiza seguro de auto por kilometro, cotizar seguro de auto, seguros de autos por kilometros, aig, seguros aig, seguros de auto aig, cotizar seguros de autos por kilometros, seguro de auto cdmx, seguro de auto en mexico, kilometro, seguros de autos, aig sxkm, seguro de auto economico'},
       { name: 'description', content: 'Ahorra en tu seguro de auto pagando por kilometro. Protege tu auto con todos los beneficios de un seguro de cobertura amplia y el respaldo de AIG.' }
     ]);
+    if (isPlatformBrowser(this.platformId)) {
+        window.location.href = 'https://seguro.sxkm.mx/seguro';
+    }
   }
   ngOnInit() {
     if (isPlatformBrowser(this.platformId)) {
