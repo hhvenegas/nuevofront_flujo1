7c2b19b9e6d1b206ca0023141f876954c2319987
paulinapopulusfintech <paulina.guerrero@populusfintech.com>
2018-10-30 13:15:59 -0600

    se eliminan componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -28,8 +28,6 @@ import { PanelpolicyComponent } from './panel/panelpolicy/panelpolicy.component'
 import { PanelcartComponent } from './panel/panelcart/panelcart.component';
 import { UsersComponent } from './users/users.component';
 import { PanelusersComponent } from './users/panelusers/panelusers.component';
-import { CarComponent } from './car/car.component';
-import { TripsComponent } from './users/trips/trips.component';
 
 const routes: Routes = [
 	{ path: 'cotiza-tu-seguro-de-auto-por-kilometro', component: HomepageComponent },
@@ -51,8 +49,6 @@ const routes: Routes = [
 	{ path: 'prensa', component: PrensaComponent },
 	{ path: 'contacto', component: ContactComponent },
 	{ path: 'user/detalles/:id_car', component: UsersComponent, canActivate: [AuthGuard] },
-	{ path: 'user/car/:id_car', component: CarComponent, canActivate: [AuthGuard]},
-	{ path: 'user/trip/:id_trip', component: TripsComponent, canActivate: [AuthGuard]},
 	{ path: 'login', component: LoginComponent},
 	{ path: 'user', component: PanelusersComponent, canActivate: [AuthGuard]},
 	{ path: 'panel', component: PanelComponent, canActivate: [AuthGuard]},
