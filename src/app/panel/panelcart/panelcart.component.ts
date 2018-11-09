7decf81c635f5c06aaca3bd941d827d7c0560d96
paulinapopulusfintech <paulina.guerrero@populusfintech.com>
2018-11-09 10:19:39 -0600

    ok

--- src/app/panel/panelcart/panelcart.component.ts
+++ src/app/panel/panelcart/panelcart.component.ts
@@ -39,9 +39,63 @@ export class PanelcartComponent implements OnInit {
     user: null,
     car: null,
     packages: null,
-    package_selected:null
+    package_selected:null,
+    package_id_selected: 250,
+    cards: null,
+    discount: 0.00,
+    suscription: 299.00,
+    total_cost: 0.00
   }
-  
+
+  payment_object = {
+    promotional_code: "",
+    token_id: "",
+    device_session_id: "",
+    paymethod: "card",
+    subscription: true,
+    invoicing: false,
+    kilometer_purchase: {
+      initial_payment: 299,
+      cost: 0.00,
+      total: 0.00,
+      kilometers: 250
+    },
+    car: {
+      id: "",
+      motor_number: "",
+      vin: "",
+      plates: ""
+    },        
+    shipping: {
+      street: "",
+      ext_number: "",
+      int_number: "",
+      suburb: "",
+      municipality: "",
+      zip_code: 11560,
+      federal_entity: ""
+      },
+      billing: {
+        zip_code: "",
+        legal_name: "",
+        rfc: ""
+      },
+      policy: {
+        first_name: "",
+        last_name: "",
+        second_last_name: "",
+        cellphone: "",
+        phone: "",
+        street: "",
+        ext_number: "",
+        int_number: "",
+        suburb: "",
+        municipality: "",
+        zip_code: null,
+        federal_entity: ""
+      }
+  }
+
   constructor(@Inject(PLATFORM_ID) private platformId: Object,private route: ActivatedRoute, private location: Location, private router: Router, private quotationService: QuotationService, private hubspotService: HubspotService, private operatorsService: OperatorsService,private spinner: NgxSpinnerService) { }
 
   ngOnInit() {
@@ -56,7 +110,12 @@ export class PanelcartComponent implements OnInit {
             user: this.quote.user,
             car: this.quote.car,
             packages: this.quote.packages_costs,
-            package_selected:this.quote.packages_costs[0]
+            package_selected:this.quote.packages_costs[0],
+            package_id_selected: 250,
+            cards: null,
+            discount:0.00,
+            suscription: 299.00,
+            total_cost: this.quote.packages_costs[0].total_cost
           }
           console.log(this.payment)
         });
@@ -70,15 +129,43 @@ export class PanelcartComponent implements OnInit {
             user: this.policy.user,
             car: this.policy.car,
             packages: null,
-            package_selected: null
+            package_selected: null,
+            package_id_selected: 250,
+            cards: null,
+            discount:0.00,
+            suscription: 299.00,
+            total_cost: 299.00
           }
           console.log(this.payment)
         });
     }
   }
 
-  setPackageSelected(package_selected){
-    this.payment.package_selected = package_selected;
+  setAction(action){
+    this.action = action;
+    if(this.action=='recarga'){
+      this.payment.suscription = 0;
+    }
+    else{
+      this.payment.suscription = 299.00;
+    }
+
+    this.payment.total_cost = this.payment.package_selected.cost_by_package+this.payment.suscription-this.payment.discount;
+  }
+
+  setPackageSelected(){
+    console.log("SE CAMBIA")
+    this.payment.packages.forEach(element => {
+      if(element.package==this.payment.package_id_selected){
+        this.payment.package_selected = element;
+        console.log("SI SON IGUALES")
+      }
+    });
+  }
+
+
+  onSubmit(){
+    
   }
 
 
