8548135210c537a2671febb733c3e032d242e7bf
paulinapopulusfintech <paulina.guerrero@populusfintech.com>
2018-12-18 17:29:19 -0600

    ok

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -16,12 +16,10 @@ const httpOptions = {
 })
 export class UsersService {
 
-	//url = 'https://dev2.sxkm.mx/api/v1/my/';
-	//url_ = 'https://dev2.sxkm.mx/api/v2/quotations/';
-	//url2 = 'https://dev2.sxkm.mx/api/v3/';
-	url = 'https://app.sxkm.mx/api/v1/my/';
-	url_ = 'https://app.sxkm.mx/api/v2/quotations/';
-	url2 = 'https://app.sxkm.mx/api/v3/';
+	url = 'https://dev2.sxkm.mx/api/v1/my/';
+	url_ = 'https://dev2.sxkm.mx/api/v2/quotations/';
+	url2 = 'https://dev2.sxkm.mx/api/v3/';
+	
 	constructor(private http: HttpClient) { }
 	getPersonalInfo(){
 		return this.http.get(this.url+"profiles/get_current_user_data", httpOptions)
