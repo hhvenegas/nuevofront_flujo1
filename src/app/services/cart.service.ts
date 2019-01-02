7da8981237cae388b16b0eda7b757e9874ddedf9
paulinapopulusfintech <paulina.guerrero@populusfintech.com>
2019-01-02 13:28:01 -0600

    ficha

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -27,7 +27,7 @@ declare var OpenPay: any;
   providedIn: 'root'
 })
 export class CartService {
-	private url = "https://dev2.sxkm.mx/v2/api/v1/web_services/";
+	private url = 'https://dev2.sxkm.mx/api/v3/';
 	public modeProd = false;
 	public openpay_prod: any = {
 		"id"      : 'mtpac6zng162oah2h67h',
@@ -49,18 +49,10 @@ export class CartService {
 	  return of(STORES);
 	}
 	
-	/** POST: add a new hero to the server */
-	sendPolicy (policy: Policy): Observable<Policy> {
-		console.log(policy);
-	    return this.http.post<Policy>(this.url+"create_payment", policy, httpOptions).pipe(
-	      tap((policy: Policy) => this.log('post policy')),
-	      catchError(this.handleError<Policy>('error post policy'))
-	    );
-	}
+	
 
-
-	getTicket(transaction_id){
-		return this.http.get(this.url+'get_transaction?transaction_id='+transaction_id)
+	getTicket(transaction_id,type){
+		return this.http.get(this.url+'payments/'+transaction_id+'?type='+type)
 		    .pipe(
 		      tap(quotation => this.log('fetched quotation')),
 		      catchError(this.handleError('getQuotation', []))
