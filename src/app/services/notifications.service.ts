38b69b8ccbb7079038c7f3faf99049db69bb52c3
paulinapopulusfintech <paulina.guerrero@populusfintech.com>
2019-02-22 13:10:15 -0600

    notificaciones

--- src/app/services/notifications.service.ts
+++ src/app/services/notifications.service.ts
@@ -4,11 +4,26 @@ import swal from 'sweetalert';
   providedIn: 'root'
 })
 export class NotificationsService {
-
+  notification: any = [
+    {
+      policy_id: 5000,
+      time: "2019-02-22T13:02:45.860Z"
+    }
+  ]
   constructor() { }
   notifications(){
-    if(localStorage.getItem("notifications")){
-      swal("Tienes una llamada a las am","","success");
-    }
+    
+    console.log("Notificaciones")
+    setInterval(()=>{
+      console.log("HOLA")
+      this.notification.forEach(element => {
+        let h = new Date(element.time)
+        let hora = new Date();
+        console.log(h)
+        if(element.time== new Date().toDateString)
+          swal("Tienes una llamada a las "+element.policy_id,"","success");
+      });
+    }, 5000);
   }
+  
 }
