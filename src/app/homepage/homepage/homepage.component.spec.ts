23ad6a638935fd9adee75dbd8669cc92b04675ed
cesar.gonzalez-at-632127757635 <cesar.gonzalez@populusfintech.com>
2019-11-12 12:52:14 -0600

    actualización sin modal

--- src/app/homepage/homepage/homepage.component.spec.ts
+++ src/app/homepage/homepage/homepage.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 //Preload
-import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
+//import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 
 
 import { HomepageComponent } from './homepage.component';
