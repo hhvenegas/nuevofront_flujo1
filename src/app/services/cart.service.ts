56cae3bce2e2a0b6b997efeda2ba2b82dc1da3cf
Cesar <cesar.gonzalez@populusfintech.com>
2020-08-26 18:27:08 -0500

    pasando a prod

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -28,8 +28,8 @@ declare var OpenPay: any;
   providedIn: 'root'
 })
 export class CartService {
-	//private url = "https://app.sxkm.mx/v2/api/v1/web_services/";
-	private url = "http://69.164.193.249/v2/api/v1/web_services/";
+	private url = "https://app.sxkm.mx/v2/api/v1/web_services/";
+	//private url = "http://69.164.193.249/v2/api/v1/web_services/";
 	
 	public modeProd = true;
 	public openpay_prod: any = {
