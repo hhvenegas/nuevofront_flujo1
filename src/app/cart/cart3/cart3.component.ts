0c5b2993e0c861cce88bcce2926555a6e5adf4ed
Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>
2020-09-21 15:22:44 -0600

    Signed-off-by: Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>

--- src/app/cart/cart3/cart3.component.ts
+++ src/app/cart/cart3/cart3.component.ts
@@ -88,6 +88,7 @@ export class Cart3Component implements OnInit {
       this.link_from_ops = true
       this.params_from_ops = params.get('buf')
       this.original_buf = params.get('buf')
+      console.log("first original_bnuf", this.original_buf)
       this.original_buf = this.original_buf.replace('%20', '+')
       console.log("original_bnuf", this.original_buf)
       console.log("parametros de ops", atob(this.params_from_ops))
